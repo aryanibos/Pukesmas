eea03cd442f9f7e14991354c43ba1d9044fe0f8a
aryanibos <aryaisnaidi01@gmail.vom>
2023-05-22 11:09:04 +0700

    Tugas Pertemuan09 Laravel::Routing - MVC

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DokterController;
 use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Halaman Pasien
+// Menampilkan data pasien
 Route::get("/pasien", [PasienController::class, 'index']);
+// Menampilkan form untuk menambahkan data pasien
 Route::get("/pasien/create", [PasienController::class, 'create']);
+// Menyimpan data pasien yang baru
 Route::post("/pasien", [PasienController::class, 'store']);
+
+// Halaman Dokter
+// Menampilkan data dokter
+Route::get("/dokter", [DokterController::class, 'index']);
+// Menampilkan form untuk menambahkan data dokter
+Route::get("/dokter/create", [DokterController::class, 'create']);
+// Menyimpan data dokter yang baru
+Route::post("/dokter", [DokterController::class, 'store']);
