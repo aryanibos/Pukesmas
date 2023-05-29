b48b942b4a016d5fad45b97349d301eeec8e3426
aryanibos <aryaisnaidi01@gmail.vom>
2023-05-29 23:00:20 +0700

    Pratikum Pertemuan10 Laravel::Templating - MVC

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DokterController;
 use App\Http\Controllers\PasienController;
+use App\Http\Controllers\RegisController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,22 +17,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/form', function () {
+    return view('form-regis.form');
 });
+
+Route::post('/form', function () {
+    return view('form-regis.form');
+});
+
+// Halaman Dashboard
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 // Halaman Pasien
 // Menampilkan data pasien
-Route::get("/pasien", [PasienController::class, 'index']);
+Route::get("/pasien", [PasienController::class, 'index'])->name('pasien');
 // Menampilkan form untuk menambahkan data pasien
-Route::get("/pasien/create", [PasienController::class, 'create']);
+Route::get("/pasien/create", [PasienController::class, 'create'])->name('pasien.create');
 // Menyimpan data pasien yang baru
-Route::post("/pasien", [PasienController::class, 'store']);
+Route::post("/pasien", [PasienController::class, 'store'])->name('pasien.simpan');
 
 // Halaman Dokter
 // Menampilkan data dokter
-Route::get("/dokter", [DokterController::class, 'index']);
+Route::get("/dokter", [DokterController::class, 'index'])->name('dokter');
 // Menampilkan form untuk menambahkan data dokter
-Route::get("/dokter/create", [DokterController::class, 'create']);
+Route::get("/dokter/create", [DokterController::class, 'create'])->name('dokter.create');
 // Menyimpan data dokter yang baru
-Route::post("/dokter", [DokterController::class, 'store']);
+Route::post("/dokter", [DokterController::class, 'store'])->name('dokter.simpan');
+
+// Route::get('/form', [RegisController::class, 'index'])->name('regist');
+// Route::post('/output', [RegisController::class, 'store'])->name('regist.simpan');
